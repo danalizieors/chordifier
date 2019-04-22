cebf10d7a1d2c9ebf5dce6f1edc074c94bb6ea6f
danalizieors <danalizieors@gmail.com>
2019-04-23 01:05:40 +0300

    Hexagon ring generation implemented

---
+++ chordifier/utils.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+CUBE_COORDINATE_DIRECTIONS = [
+    (-1, +1),
+    (0, +1),
+    (+1, 0),
+    (+1, -1),
+    (0, -1),
+    (-1, 0),
+]
+
+
+def hexagon_ring(radius, right=False):
+    start = np.array((0, -radius))
+    directions = np.array(CUBE_COORDINATE_DIRECTIONS)
+    ring = walk(start, directions, radius)
+
+    if right:
+        return ring[0:1] + ring[:0:-1]
+    else:
+        return ring
+
+
+def walk(start, directions, steps_in_each_direction):
+    current = start
+    steps = []
+
+    for direction in directions:
+        for step in range(steps_in_each_direction):
+            steps.append(current)
+            current = current + direction
+
+    return steps
