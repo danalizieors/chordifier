81608e29ce4e42739382032d83e58d417592d8e5
danalizieors <danalizieors@gmail.com>
2019-04-24 20:22:18 +0300

    Operations refactored to use Numpy

--- chordifier/Zone.py
+++ chordifier/Zone.py
@@ -1,70 +1,36 @@
-import chordifier.utils as u
+import numpy as np
 
-from chordifier.utils import hexagon_ring, vector
-
-MIDDLE_RING_KEYS = [
-    [vector(0, -1)],
-    [vector(0, 1)],
-    [vector(0, -2)],
-]
-
-ZONES = {
-    'thumb': {
-        'position': vector(3, 0),
-        'keys': [
-            hexagon_ring(1),
-            hexagon_ring(2)[:-2],
-            hexagon_ring(2)[-2:],
-        ],
-    },
-    'index': {
-        'position': vector(5, -3),
-        'keys': [
-            hexagon_ring(1)[0:2],
-            hexagon_ring(1)[2:4],
-            hexagon_ring(2)[0:2],
-        ],
-    },
-    'middle': {
-        'position': vector(6, -4),
-        'keys': MIDDLE_RING_KEYS,
-    },
-    'ring': {
-        'position': vector(7, -4),
-        'keys': MIDDLE_RING_KEYS,
-    },
-    'little': {
-        'position': vector(8, -4),
-        'keys': [
-            hexagon_ring(1, True)[0:2],
-            hexagon_ring(1, True)[2:4],
-            hexagon_ring(2, True)[0:2],
-            hexagon_ring(2, True)[2:4],
-        ],
-    },
-}
+from chordifier.configs import ZONES
 
 
 class Zone:
-    def __init__(self, name, level, right=False):
+    def __init__(self, name: str, level: int, right: bool = False):
         self.name = name
+        self.right = right
         self.keys = generate_keys(ZONES[name], level, right)
 
 
-def generate_keys(zone, level, right):
+def generate_keys(zone: dict, level: int, right: bool):
     position = zone['position']
     keys = define_keys(zone, level)
-    translated_keys = u.translate(position, keys)
+    translated_keys = position + keys
 
     if right:
-        return u.mirror(translated_keys)
+        return mirror(translated_keys)
     else:
         return translated_keys
 
 
-def define_keys(zone, level):
-    origin = vector(0, 0)
-    keys = zone['keys']
-    keys_for_level = keys[0:level]
+def define_keys(zone: dict, level: int):
+    origin = np.array([0, 0])
+    possible_keys = zone['keys']
+    keys_for_level = [[origin]] + possible_keys[0:level]
 
-    return sum(keys_for_level, [origin])
+    return np.concatenate(keys_for_level)
+
+
+def mirror(coordinates):
+    inverted_x = np.array([-1, 1]) * coordinates
+    zeroed_y = np.array([1, 0]) * coordinates
+    x_to_y = np.roll(zeroed_y, 1, 1)
+    return inverted_x + x_to_y
--- chordifier/utils.py
+++ chordifier/utils.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 
-def vector(x, y):
-    return np.array([x, y])
+def vector(*l):
+    return np.array(l)
 
 
 CUBE_COORDINATE_DIRECTIONS = [
@@ -20,7 +20,7 @@ def hexagon_ring(radius, right=False):
     ring = walk(start, CUBE_COORDINATE_DIRECTIONS, radius)
 
     if right:
-        return ring[0:1] + ring[:0:-1]
+        return np.concatenate([ring[0:1], ring[:0:-1]])
     else:
         return ring
 
@@ -34,17 +34,4 @@ def walk(start, directions, steps_in_each_direction):
             steps.append(current)
             current = current + direction
 
-    return steps
-
-
-def translate(position, coordinates):
-    return [position + coordinate for coordinate in coordinates]
-
-
-def mirror(coordinates):
-    return [mirror_coordinate(coordinate) for coordinate in coordinates]
-
-
-def mirror_coordinate(coordinate):
-    q, r = coordinate
-    return vector(-q, q + r)
+    return np.array(steps)
