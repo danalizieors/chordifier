cc002a5a151d18388e1b9967fd52f1393b50f0af
danalizieors <danalizieors@gmail.com>
2019-08-18 18:58:43 +0300

    Keyboard rendering can show button indices and positions

--- chordifier/KeyboardRenderer.py
+++ chordifier/KeyboardRenderer.py
@@ -22,6 +22,8 @@ class KeyboardRenderer:
         self.qs = keys[:, 0]
         self.rs = keys[:, 1]
         self.colors = match_colors(keyboard.zones)
+        self.indices = extract_indices(keyboard.zones)
+        self.positions = extract_positions(keyboard.zones)
 
 
 def aggregate_keys(zones):
@@ -32,3 +34,11 @@ def match_colors(zones):
     zones_with_colors = zip(zones, PALETTE)
     colors = [len(zone.keys) * [color] for zone, color in zones_with_colors]
     return np.concatenate(colors)
+
+
+def extract_indices(zones):
+    return [index + 1 for zone in zones for index in range(len(zone.keys))]
+
+
+def extract_positions(zones):
+    return [key for zone in zones for key in zone.keys]
